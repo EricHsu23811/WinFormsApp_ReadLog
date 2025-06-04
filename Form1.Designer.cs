6d66cf3d95cee3ba79a3681555f7d78079ecf114
EricHsu23811 <eric1491@gmail.com>
2025-06-04 14:59:48 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace WinFormsApp_ReadLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            settingsToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            btnSelect = new Button();
+            txtFilePath = new TextBox();
+            lblPcMac = new Label();
+            txtMacAddr = new TextBox();
+            btnRead = new Button();
+            grpSearch = new GroupBox();
+            lblResult = new Label();
+            lblKeyword = new Label();
+            txtKeyword = new TextBox();
+            richText_Ans = new RichTextBox();
+            lblSWver = new Label();
+            saveFileDialog1 = new SaveFileDialog();
+            menuStrip1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            grpSearch.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, settingsToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(782, 27);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(47, 23);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(116, 26);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // settingsToolStripMenuItem
+            // 
+            settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            settingsToolStripMenuItem.Size = new Size(80, 23);
+            settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 23);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(134, 26);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.ImageScalingSize = new Size(20, 20);
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 328);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(782, 25);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(158, 19);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // btnSelect
+            // 
+            btnSelect.Location = new Point(26, 53);
+            btnSelect.Name = "btnSelect";
+            btnSelect.Size = new Size(149, 32);
+            btnSelect.TabIndex = 2;
+            btnSelect.Text = "Select File";
+            btnSelect.UseVisualStyleBackColor = true;
+            btnSelect.Click += btnSelect_Click;
+            // 
+            // txtFilePath
+            // 
+            txtFilePath.Location = new Point(26, 91);
+            txtFilePath.Name = "txtFilePath";
+            txtFilePath.Size = new Size(742, 27);
+            txtFilePath.TabIndex = 3;
+            txtFilePath.TextChanged += txtFilePath_TextChanged;
+            // 
+            // lblPcMac
+            // 
+            lblPcMac.AutoSize = true;
+            lblPcMac.Location = new Point(509, 60);
+            lblPcMac.Name = "lblPcMac";
+            lblPcMac.Size = new Size(81, 19);
+            lblPcMac.TabIndex = 4;
+            lblPcMac.Text = "PC MAC：";
+            // 
+            // txtMacAddr
+            // 
+            txtMacAddr.Location = new Point(596, 57);
+            txtMacAddr.Name = "txtMacAddr";
+            txtMacAddr.Size = new Size(172, 27);
+            txtMacAddr.TabIndex = 5;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(596, 279);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(172, 44);
+            btnRead.TabIndex = 6;
+            btnRead.Text = "GetResult";
+            btnRead.UseVisualStyleBackColor = true;
+            btnRead.Click += btnRead_Click;
+            // 
+            // grpSearch
+            // 
+            grpSearch.Controls.Add(lblResult);
+            grpSearch.Controls.Add(lblKeyword);
+            grpSearch.Controls.Add(txtKeyword);
+            grpSearch.Controls.Add(richText_Ans);
+            grpSearch.Location = new Point(20, 124);
+            grpSearch.Name = "grpSearch";
+            grpSearch.Size = new Size(748, 149);
+            grpSearch.TabIndex = 8;
+            grpSearch.TabStop = false;
+            grpSearch.Text = "Search Test";
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(56, 62);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(67, 19);
+            lblResult.TabIndex = 12;
+            lblResult.Text = "Result：";
+            // 
+            // lblKeyword
+            // 
+            lblKeyword.AutoSize = true;
+            lblKeyword.Location = new Point(37, 34);
+            lblKeyword.Name = "lblKeyword";
+            lblKeyword.Size = new Size(86, 19);
+            lblKeyword.TabIndex = 11;
+            lblKeyword.Text = "Keyword：";
+            // 
+            // txtKeyword
+            // 
+            txtKeyword.Location = new Point(129, 26);
+            txtKeyword.Name = "txtKeyword";
+            txtKeyword.Size = new Size(149, 27);
+            txtKeyword.TabIndex = 10;
+            txtKeyword.TextChanged += txtKeyword_TextChanged;
+            // 
+            // richText_Ans
+            // 
+            richText_Ans.Location = new Point(129, 59);
+            richText_Ans.Name = "richText_Ans";
+            richText_Ans.Size = new Size(613, 84);
+            richText_Ans.TabIndex = 0;
+            richText_Ans.Text = "";
+            // 
+            // lblSWver
+            // 
+            lblSWver.AutoSize = true;
+            lblSWver.Location = new Point(20, 292);
+            lblSWver.Name = "lblSWver";
+            lblSWver.Size = new Size(103, 19);
+            lblSWver.TabIndex = 9;
+            lblSWver.Text = "SW_Ver_Date";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 353);
+            Controls.Add(lblSWver);
+            Controls.Add(grpSearch);
+            Controls.Add(btnRead);
+            Controls.Add(txtMacAddr);
+            Controls.Add(lblPcMac);
+            Controls.Add(txtFilePath);
+            Controls.Add(btnSelect);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "STgui Log *.html read test";
+            FormClosing += Form1_FormClosing;
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            grpSearch.ResumeLayout(false);
+            grpSearch.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private StatusStrip statusStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem settingsToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private Button btnSelect;
+        private TextBox txtFilePath;
+        private Label lblPcMac;
+        private TextBox txtMacAddr;
+        private Button btnRead;
+        private GroupBox grpSearch;
+        private RichTextBox richText_Ans;
+        private Label lblSWver;
+        private SaveFileDialog saveFileDialog1;
+        private TextBox txtKeyword;
+        private Label lblKeyword;
+        private Label lblResult;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,274 @@
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.IO;
+using System.IO.Ports;
+
+namespace WinFormsApp_ReadLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*******************************************************************************
+         * Constant 
+         ******************************************************************************/
+        /* These are constant variables */
+        public static readonly string userDocFolder = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        /*******************************************************************************
+         * Gobal Variable 
+         ******************************************************************************/
+        static string userAppFolder = userDocFolder + "\\pxToRGB";
+        // 可選：獲取完整路徑
+        string executablePath = Assembly.GetExecutingAssembly().Location;
+        // 獲取目前執行檔的名稱
+        static string fileName = Assembly.GetExecutingAssembly().GetName().Name;
+        //獲取執行檔版本
+        string fileVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        string buildDateTime = System.IO.File.GetCreationTime(Assembly.GetExecutingAssembly().Location).ToString("yyyy-MM-dd HH:mm");    //ToString("yyyy-MM-dd HH:mm:ss")
+
+        string strFilePath = "";    //userAppFolder + "\\" + "H4_IQC_LBS_BURN.csv";
+        static string strDailyLogDate = DateTime.Now.ToString("yyyy-MM-dd");
+        static string strFileDailyFolder = userAppFolder + "\\" + DateTime.Now.ToString("yyyy")
+            + "\\" + DateTime.Now.ToString("MM") + "\\" + DateTime.Now.ToString("dd");
+        static string strLogNameDaily = ""; //strFileDailyFolder + "\\" + "H4_IQC_LBS_BURN" + "_" + strDailyLogDate + ".csv";
+        string strFileDailyPath = "";   //userAppFolder + "\\" + strLogNameDaily;    //@"\Burn1to10.csv";
+        string strSwVer = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion.ToString();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMacAddr.ReadOnly = true;
+            txtFilePath.ReadOnly = true;
+            //txtFilePath.BackColor = SystemColors.Info;
+            richText_Ans.ReadOnly = true;
+            toolStripStatusLabel1.Text = "Select a file to Read.";
+            btnRead.Enabled = false; txtKeyword.Enabled = false;
+
+            txtMacAddr.Text = GetMacAddress().ToString();
+            lblSWver.Text = "SW : " + fileName + " - ver: " + strSwVer + " ; 程式碼日期 : " + buildDateTime;
+        }
+
+        public static string GetMacAddress()
+        {
+            string macAddresses = "";
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet)  //2023-03-21
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+                else if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            return macAddresses;
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            utilHelpAbout();
+        }
+        private void utilHelpAbout()
+        {
+            string company = "Megaforce(R) " + fileName + " Ver:" + fileVersion + "\n";
+            string copyright = "(C)Copyright Megaforce Corp 2025-2035.\n";
+            string buildDT = "Built on " + buildDateTime + "\n";
+            string Author = "Author: Eric Hsu\n";
+            MessageBox.Show(company + copyright + buildDT + Author, "About " + fileName);
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void utilCheckUserFile()
+        {
+            if (Directory.Exists(userAppFolder) == false)
+            { Directory.CreateDirectory(userAppFolder); }
+            //if (Directory.Exists(strFileDailyFolder) == false)
+            //{ Directory.CreateDirectory(strFileDailyFolder); }
+        }
+        public bool IsFileLocked(string filename)
+        {
+            bool Locked = false;
+            try
+            {
+                FileStream fs =
+                    File.Open(filename, FileMode.OpenOrCreate,
+                    FileAccess.ReadWrite, FileShare.None);
+                fs.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Locked = true;
+            }
+            return Locked;
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure to exit？", "Question", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+
+            }
+            else { e.Cancel = true; }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_DEVICECHANGE = 0x219;
+            const int DBT_DEVICEARRIVAL = 0x8000;
+            const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_CLOSE = 0xF060;
+            const int SC_CLICK = 0xF093;
+            const int SC_DOUBLE_CLICK = 0xF063;
+
+            object ojb = new object();
+            try   //2023-09-25
+            {
+                // WM_DEVICECHANGE Message : 電腦硬體裝置改變時產生的訊息
+                if (m.Msg == WM_DEVICECHANGE)
+                {
+                    switch (m.WParam.ToInt32())
+                    {
+                        case WM_DEVICECHANGE:
+                            Console.WriteLine("DEVICE CHANGEed");
+                            break;
+                        // DBT_DEVICEARRIVAL Event : 裝置插入並且可以使用時，產生的系統訊息
+                        case DBT_DEVICEARRIVAL:
+                            //string[] portnames = SerialPort.GetPortNames(); //---工具箱無SerialPort，不存在於目前的內容中
+                            Console.WriteLine("DEVICE was inserted.");
+                            toolStripStatusLabel1.Text = "DEVICE was inserted";
+                            break;
+
+                        // DBT_DEVICEREMOVECOMPLETE Event : 裝置卸載或移除時產生的系統訊息
+                        case DBT_DEVICEREMOVECOMPLETE:
+                            //portnames = SerialPort.GetPortNames(); //---工具箱無SerialPort，不存在於目前的內容中
+                            Console.WriteLine("DEVICE was removed.");
+                            toolStripStatusLabel1.Text = "DEVICE was removed";
+                            break;
+                    }
+                }
+                else if (m.Msg == WM_SYSCOMMAND)
+                {
+                    if (m.WParam.ToInt32() == SC_CLOSE)
+                    {
+                        // 点击winform右上关闭按钮 
+                        // 加入想要的逻辑处理
+                        //utilWindowClosingWithConfirm(false);
+                        //return;
+                    }
+                    else if (m.WParam.ToInt32() == SC_CLICK)
+                    {
+                        return;
+                    }
+                    else if (m.WParam.ToInt32() == SC_DOUBLE_CLICK)
+                    {
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "WndProc");
+            }
+            base.WndProc(ref m);
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Title = "Select A File";
+            openDialog.Filter = "Log Files (*.txt;*.log;*.json;*.html)|*.txt; *.log; *.json;*.html" + "|" + "All Files (*.*)|*.*";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = openDialog.FileName;
+                txtFilePath.Text = file;
+                toolStripStatusLabel1.Text = "InputFile= " + txtFilePath.Text;
+            }
+            if (txtFilePath.Text != "" ) 
+            { 
+                txtKeyword.Enabled = true; 
+                if(txtKeyword.Text != "") {toolStripStatusLabel1.Text = "Keyword已輸入，可按'GetResult'";  }
+                else { toolStripStatusLabel1.Text = "請輸入Keyword已輸入"; }
+            }
+            else 
+            { txtKeyword.Enabled = false; toolStripStatusLabel1.Text = "請輸入Keywords"; }
+            
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtKeyword.Text != "") 
+            { 
+                btnRead.Enabled = true;
+                toolStripStatusLabel1.Text = "輸入完成請按'GetResult'.";
+            }
+            else { btnRead.Enabled = false; toolStripStatusLabel1.Text = "請輸入Keywords"; }
+            richText_Ans.Text = "";
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string filePath = txtFilePath.Text; // 請替換成你的檔案路徑
+            string targetString = txtKeyword.Text; // 請替換成你要搜尋的字串
+            string strAnswer = "";
+            try
+            {
+                int count = 0;
+                Boolean bStartFindResult = false;   //start to find certain value
+                
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line; 
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        if (bStartFindResult)
+                        {
+                            if (line.Contains("Pass")) 
+                            { strAnswer = "Pass"; bStartFindResult = false; }
+                            else if(line.Contains("Fail")) 
+                            { strAnswer = "Fail"; bStartFindResult = false; }
+                            else if (line.Contains("EndScript")) 
+                            { bStartFindResult = false; }
+                        }
+                        if (line.Contains(targetString))
+                        {
+                            Console.WriteLine($"Found '{targetString}' in line: {line}");
+                            // 找到字串後，可以執行其他操作
+                            //break; // 如果只需要找到第一個匹配項，則在此處中斷迴圈
+                            count += 1;
+                            bStartFindResult = true;
+                        }
+                        //count += line.ToLower().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Count(word => word.Equals(targetString.ToLower())); //忽略大小寫比較
+                        Console.WriteLine(line);
+                    }
+                }
+                Console.WriteLine($"The string '{targetString}' appears {count} times in the file.");
+                richText_Ans.Text = "Found keyword: '" + targetString + "', count = " + count.ToString() + "\r\n" + "Answer is " + strAnswer + ".";
+                toolStripStatusLabel1.Text = "完成查詢，結果為 " + strAnswer;
+            }
+            catch (FileNotFoundException)
+            { Console.WriteLine($"File not found: {filePath}"); }
+            catch (Exception ex)
+            { Console.WriteLine($"An error occurred: {ex.Message}"); }
+        }
+        private void txtFilePath_TextChanged(object sender, EventArgs e)
+        {
+            richText_Ans.Text = "";
+        }
+    }
+}
